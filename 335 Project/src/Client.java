8ef0e8e0924c819a13b2a2035924545dee32169f
jonathanh <jonathanhouge@email.arizona.edu>
2022-11-16 22:45:32 -0700

    set 'er up!

---
+++ 335 Project/src/Client.java
@@ -0,0 +1,9 @@
+import org.eclipse.swt.widgets.Display;
+
+public class Client {
+	
+	public static void main(String[] args) {
+		System.out.println("let's test");
+		Display test = new Display();
+	}
+}
