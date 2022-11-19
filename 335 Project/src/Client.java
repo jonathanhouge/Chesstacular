1d9138af5021c1fe73424f246304a9d3d77edff7
Ali Sartaz Khan <71156712+alisartazkhan@users.noreply.github.com>
2022-11-18 21:13:46 -0700

    Delete Client.java

--- 335 Project/src/Client.java
+++
@@ -1,9 +0,0 @@
-import org.eclipse.swt.widgets.Display;
-
-public class Client {
-	
-	public static void main(String[] args) {
-		System.out.println("let's test");
-		Display test = new Display();
-	}
-}
