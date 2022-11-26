53ec33f389c47e060da08592683e9b4557ba45d2
jonathanh <jonathanhouge@email.arizona.edu>
2022-11-26 09:37:15 -0700

    tile class created

---
+++ 335 Project/src/Tile.java
@@ -0,0 +1,37 @@
+/* Tile class. Each tile represents a square on the chess board.
+ * 
+ * Author: Jonathan Houge
+ */
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Rectangle;
+
+import pieces.Piece;
+
+public class Tile {
+	
+	private Rectangle tile;
+	private Piece piece;
+	private Color color;
+	private String location;
+	
+	public Tile(Color color, int x, int y) {
+		this.piece = null;
+		this.color = color;
+		this.tile = new Rectangle(x, y, 0, 0); }
+	
+	public void draw(GC gc) {
+		gc.setBackground(color);
+		gc.fillRectangle(tile); }
+	
+	// setters
+	public void setPiece(Piece piece) { this.piece = piece; }
+	public void setColor(Color color) { this.color = color; }
+
+	// getters
+	public Piece getPiece() { return this.piece; }
+	public Color getColor() { return this.color; }
+
+}
